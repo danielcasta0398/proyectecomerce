245e3ec2202eba0ed83fa9a7e741245f89330bd6
juancastano0398 <juancastano0398@gmail.com>
2022-04-15 19:01:17 +0200

    errores

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import CardProducts from '../components/CardProducts';
 import Header from '../components/Header';
 import {  getCategoriesThunk, getProductsThunk } from '../redux/actions';
